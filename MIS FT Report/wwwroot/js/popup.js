629c8dfba818e3fa02a1b2b211fd09606ec6deec
Rudrakshpnd <rudrabhakt.3@gmail.com>
2024-02-26 11:07:21 +0530

    Popup Changes in Department Create button

---
+++ MIS FT Report/wwwroot/js/popup.js
@@ -0,0 +1,21 @@
+﻿var createButton = document.getElementById("createbtn");
+
+var modal = document.getElementById("popup-modal");
+
+var closeButton = document.getElementsByClassName("close")[0];
+
+
+createButton.onclick = function () {
+    modal.style.display = "block";
+
+}
+
+closeButton.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
